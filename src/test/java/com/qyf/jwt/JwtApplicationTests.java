ebc9431aa415dc609d8cac060380615e05339e75
superMelo <774876970@qq.com>
2021-06-16 15:35:12 +0800

    利用redis的bitop实现检查用户连续签到

--- src/test/java/com/qyf/jwt/JwtApplicationTests.java
+++ src/test/java/com/qyf/jwt/JwtApplicationTests.java
@@ -1,7 +1,6 @@
 package com.qyf.jwt;
 
 import com.alibaba.fastjson.JSON;
-import com.qyf.jwt.cache.RedisCache;
 import com.qyf.jwt.cache.TokenCache;
 import com.qyf.jwt.cache.UserCache;
 import com.qyf.jwt.entity.Token;
@@ -10,46 +9,44 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.connection.BitFieldSubCommands;
 import org.springframework.data.redis.connection.RedisStringCommands;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest
 @Slf4j
 class JwtApplicationTests {
 
-	@Autowired
-	TokenCache tokenCache;
+    @Autowired
+    TokenCache tokenCache;
 
-	@Autowired
-	UserCache userCache;
+    @Autowired
+    UserCache userCache;
 
-	@Autowired
-	private RedisTemplate redisTemplate;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	void pushList(){
-		String id = UUID.randomUUID().toString();
-		Token token = Token.builder().id(id)
-				.userId("1").token(id + "_1").expTime(new Date()).ip("127.0.0.1").build();
-		tokenCache.pushList("list", token);
-		token = tokenCache.popList("list");
-		log.info("token:{}", token);
-	}
+    @Test
+    void pushList() {
+        String id = UUID.randomUUID().toString();
+        Token token = Token.builder().id(id)
+                .userId("1").token(id + "_1").expTime(new Date()).ip("127.0.0.1").build();
+        tokenCache.pushList("list", token);
+        token = tokenCache.popList("list");
+        log.info("token:{}", token);
+    }
 
 //	@Test
 //	void popList(){
@@ -57,67 +54,87 @@ class JwtApplicationTests {
 //		log.info("token:{}", token);
 //	}
 
-	@Test
-	void loadData(){
-		User user = User.builder().id(UUID.randomUUID().toString())
-				.username("qyf").password("123").build();
-		userCache.pushList("user", user);
-		List<User> users = userCache.findAll("user");
-		log.info("users:{}", users);
-	}
+    @Test
+    void loadData() {
+        User user = User.builder().id(UUID.randomUUID().toString())
+                .username("qyf").password("123").build();
+        userCache.pushList("user", user);
+        List<User> users = userCache.findAll("user");
+        log.info("users:{}", users);
+    }
 
 
-	@Test
-	void zadd(){
-		userCache.zadd("users", "1", 1L);
-		userCache.zadd("users", "2", 2L);
-		userCache.zadd("users", "3", 3L);
-		userCache.zadd("users", "4", 1L);
-		userCache.zadd("users", "5", 0L);
-		Set<ZSetOperations.TypedTuple> key = userCache.getScore("users");
-		log.info("users:{}", JSON.toJSONString(key));
-	}
+    @Test
+    void zadd() {
+        userCache.zadd("users", "1", 1L);
+        userCache.zadd("users", "2", 2L);
+        userCache.zadd("users", "3", 3L);
+        userCache.zadd("users", "4", 1L);
+        userCache.zadd("users", "5", 0L);
+        Set<ZSetOperations.TypedTuple> key = userCache.getScore("users");
+        log.info("users:{}", JSON.toJSONString(key));
+    }
 
-	//模拟签到
-	@Test
-	void setBit(){
-		Date date = new Date();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		String time = format.format(date);
-		for (int i = 0; i < 7; i++) {
-			if (i == 3){
-				continue;
-			}else {
-				userCache.addBit(time, Long.valueOf(i), true);
-			}
-		}
-		Long count = userCache.bitCount(time);
-		log.info("签到几天:{}天", count);
-	}
+    //模拟签到
+    @Test
+    void setBit() {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String time = format.format(date);
+        for (int i = 0; i < 7; i++) {
+            if (i == 3) {
+                continue;
+            } else {
+                userCache.addBit(time, Long.valueOf(i), true);
+            }
+        }
+        Long count = userCache.bitCount(time);
+        log.info("签到几天:{}天", count);
+    }
 
-	//清除签到记录
-	@Test
-	void clearBit(){
-		String str = (String)redisTemplate.opsForList().rightPop("user_bit_1");
-		redisTemplate.delete(str);
-	}
+    //清除签到记录
+    @Test
+    void clearBit() {
+        String str = (String) redisTemplate.opsForList().rightPop("user_bit_1");
+        redisTemplate.delete(str);
+    }
 
-	//检查用户连续签到
-	@Test
-	void check(){
-		Long count = (Long)redisTemplate.execute((RedisCallback<Long>) con -> con.bitOp(RedisStringCommands.BitOperation.AND,
-				"test1".getBytes(), "login:20210325".getBytes(), "login:20210326".getBytes()));
-		log.info("count:{}", count);
-		Boolean test1 = userCache.getBit("test1", 3L);
+    //检查用户连续签到
+    @Test
+    void check() {
+        Long count = (Long) redisTemplate.execute((RedisCallback<Long>) con -> con.bitOp(RedisStringCommands.BitOperation.AND,
+                "test1".getBytes(), "login:20210325".getBytes(), "login:20210326".getBytes()));
+        log.info("count:{}", count);
+        Boolean test1 = userCache.getBit("test1", 3L);
 //		System.out.println(redisTemplate.expire("test1", 5, TimeUnit.SECONDS));
-		System.out.println(redisTemplate.getExpire("test1"));
-		log.info("state:{}", test1);
-	}
+        System.out.println(redisTemplate.getExpire("test1"));
+        log.info("state:{}", test1);
+    }
 
-	@Test
-	void expire(){
-		redisTemplate.opsForValue().set("test2", "1");
-		System.out.println(redisTemplate.expire("test2", 5, TimeUnit.SECONDS));
-		System.out.println(redisTemplate.getExpire("test2"));
-	}
+    @Test
+    void expire() {
+        redisTemplate.opsForValue().set("test2", "1");
+        System.out.println(redisTemplate.expire("test2", 5, TimeUnit.SECONDS));
+        System.out.println(redisTemplate.getExpire("test2"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        Semaphore semaphore = new Semaphore(1);
+        AtomicInteger count = new AtomicInteger(0);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    count.incrementAndGet();
+                    System.out.println("执行业务");
+                    System.out.println(count.get());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    semaphore.release();
+                }
+            }).start();
+        }
+    }
+
 }
